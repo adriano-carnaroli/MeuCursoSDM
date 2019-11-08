092c71818b4add386d7792b0415a720e0c555c60
Adriano <driano.carnaroli@zarpsystem.com.br>
2019-11-08 13:48:28 -0300

    Initial commit - Criando o projeto do roteiro Meu Curso SDM

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/model/Disciplina.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.scl.sdm.model
+
+// Valores padrões são Strings vazias
+data class Disciplina(
+    val codigo: String ="",
+    var nome: String = "",
+    var ementa: String = "")
